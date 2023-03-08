50d6b7456d58d46b51af182bb929f161d83c51fc
Масляник Сергій Олегович <s.maslianyk@copter.kiev.office>
2023-03-08 11:32:02 +0200

    upd важкоувато

--- test.js
+++ test.js
@@ -2,4 +2,6 @@ const test = "test";
 alert("Hello WORLD");
 12313131131123312
 p1 fjsgjsgpojgsgoj hog ihgoirg ogfwefw
+P2 OTOTOTOTOTOTOTOOTOTOTOTOTOTOTTOTO
 
+SDFSDF;SDFSDFSDFSF;SDFSDFS;FSDFSF;SFSFSDF;S;
